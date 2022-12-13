131bacbf189b227de0f011c33a1eda6b7178851b
sandra231092 <109413934+sandra231092@users.noreply.github.com>
2022-12-13 17:54:14 +0500

    Add files via upload

---
+++ task15.py
@@ -0,0 +1,22 @@
+#Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+#Пример:
+#- для k = 8 список будет выглядеть так: 
+#[-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+
+n = int(input('Введите число: '))
+
+def fibonacci(n):
+    fibo_nums = []
+    a,b = 1,1
+    for i in range(n - 1):
+        fibo_nums.append(a)
+        a,b = b,a+b
+    a,b = 0,1
+    for i in range(n):
+        fibo_nums.insert(0,a)
+        a,b = b,a-b
+    return fibo_nums
+
+fibo_nums = fibonacci(n)
+print(fibonacci(n))
+print(fibo_nums.index(0))
